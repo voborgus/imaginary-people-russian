a4c1a6ee982400829d02485e8dddb6471cef2aff
Dmitrii Sugrobov <voborgus@gmail.com>
2024-11-10 22:40:06 +0300

    metrica

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import YandexMetrika from '../components/Metrica'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <YandexMetrika />
+      <Component {...pageProps} />
+    </>
+  )
 }
